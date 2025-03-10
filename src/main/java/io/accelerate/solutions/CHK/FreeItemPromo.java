0904b2bfe520913616d42641cacac250575d9b01
sbx_user1051 <sbx_user1051@169.254.66.171>
2025-03-10 16:14:29 +0000

    Mon Mar 10 16:14:29 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/FreeItemPromo.java
+++ src/main/java/io/accelerate/solutions/CHK/FreeItemPromo.java
@@ -5,9 +5,9 @@ import java.util.Map;
 public class FreeItemPromo implements CrossPromotion {
     private final Character item;
     private final int requiredQuantity;
-    private final Map<String, Integer> freeitems;
+    private final Map<Character, Integer> freeitems;
 
-    public FreeItemPromo(Character item, int requiredQuantity, Map<String, Integer> freeitems) {
+    public FreeItemPromo(Character item, int requiredQuantity, Map<Character, Integer> freeitems) {
         this.item = item;
         this.requiredQuantity = requiredQuantity;
         this.freeitems = freeitems;
@@ -21,9 +21,21 @@ public class FreeItemPromo implements CrossPromotion {
     @Override
     public void apply(Map<Character, Integer> checkOutItems) {
         int promoItemCount = checkOutItems.get(item);
-
         int required = promoItemCount / requiredQuantity;
+        if (required <= 0) {
+            return;
+        }
+
+        for (Map.Entry<Character, Integer> entry : freeitems.entrySet()) {
+            Character freeItem = entry.getKey();
+            int freeQuantity = entry.getValue();
+            int totalFree = required * freeQuantity;
+            int currentCount = checkOutItems.getOrDefault(freeItem, 0);
+
+            checkOutItems.put(freeItem, Math.max(currentCount - totalFree, totalFree));
+        }
     }
 }
+
 
 
