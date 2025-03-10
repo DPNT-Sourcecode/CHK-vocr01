acb526733663ae6742f3b23ae4b6918dadfd3c3c
sbx_user1051 <sbx_user1051@169.254.66.171>
2025-03-10 17:44:29 +0000

    Mon Mar 10 17:44:29 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/PromoConfig.java
+++ src/main/java/io/accelerate/solutions/CHK/PromoConfig.java
@@ -3,6 +3,8 @@ package io.accelerate.solutions.CHK;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class PromoConfig {
 
@@ -14,11 +16,19 @@ public class PromoConfig {
             'E', 40
     );
 
+    private static final String VALID_ITEMS_REGEX = "[" +
+            PRICES.keySet().
+                    stream().
+                    map(String::valueOf).
+                    collect(Collectors.joining()) + "]*";
+
+    public static final Pattern VALID_ITEMS_PATTERN = Pattern.compile(VALID_ITEMS_REGEX);
+
     public static Integer getUnitPrice(Character item) {
         return PRICES.get(item);
     }
 
-    public static List<MultiPricePromo> getPromotionForItem(Character item) {
+    public static List<MultiPricePromo> getMultiPricePromotionForItem(Character item) {
         List<MultiPricePromo> promotions = new ArrayList<MultiPricePromo>();
 
         switch (item) {
@@ -27,7 +37,20 @@ public class PromoConfig {
                 promotions.add(new MultiPricePromo('A', 5, 200));
                 break;
             case 'B':
-                promotions.add(new MultiPricePromo('B', 2, 45));
+                break;
+            default:
+                break;
         }
+
+        return promotions;
+    }
+
+    public static List<FreeItemPromo> getFreeItemPromotionForItem(Character item) {
+        List<FreeItemPromo> promotions = new ArrayList<FreeItemPromo>();
+
+        promotions.add(new FreeItemPromo('E', 2, Map.of('B', 1)));
+
+        return promotions;
     }
 }
+
