d02592c57810c8e4f55ac087a327a9ddc09cf840
sbx_user1051 <sbx_user1051@169.254.66.171>
2025-03-10 17:29:29 +0000

    Mon Mar 10 17:29:29 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/dictionary/ItemPromoDictionary.java
+++ src/main/java/io/accelerate/solutions/CHK/dictionary/ItemPromoDictionary.java
@@ -1,8 +1,21 @@
 package io.accelerate.solutions.CHK.dictionary;
 
+import io.accelerate.solutions.CHK.MultiPricePromo;
+import io.accelerate.solutions.CHK.Promotion;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.TreeMap;
 
 public class ItemPromoDictionary {
-    public static TreeMap<Character, Integer> itemPriceDictionary = new TreeMap<>();
+    public static TreeMap<Character, List<Promotion>> itemPromoDictionary = new TreeMap<>();
 
+    static {
+        itemPromoDictionary.put('A', List.of(
+                new MultiPricePromo('A', 3, 130),
+                new MultiPricePromo('A', 5, 200)
+        ));
+        itemPromoDictionary.put('B', List.of(new MultiPricePromo('B', 2, 45)));
+    }
 }
+
