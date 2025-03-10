b6750136b035e35bfcafa99f6d8dc8fb4480a1ae
sbx_user1051 <sbx_user1051@169.254.66.171>
2025-03-10 12:19:18 +0000

    Mon Mar 10 12:19:18 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,25 @@
 package io.accelerate.solutions.CHK;
 
-import io.accelerate.runner.SolutionNotImplementedException;
+import java.util.Map;
 
 public class CheckoutSolution {
+
+    private static final Map<Character, Integer> PRICES = Map.of(
+            'A', 50,
+            'B', 30,
+            'C', 20,
+            'D', 15
+    );
+
+    private static final Map<Character, int[]> PROMOTIONS = Map.of(
+            'A', new int[]{3,130},
+            'B', new int[]{2,45}
+    );
+
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        if (skus == null || skus.isBlank()) {
+            return -1;
+        }
     }
 }
+
