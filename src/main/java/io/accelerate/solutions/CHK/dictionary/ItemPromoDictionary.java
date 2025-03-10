4735b14ea06a37473a07ca77993ddca2007eec65
sbx_user1051 <sbx_user1051@169.254.66.171>
2025-03-10 16:17:29 +0000

    Mon Mar 10 16:17:29 UTC 2025

---
+++ src/main/java/io/accelerate/solutions/CHK/dictionary/ItemPromoDictionary.java
@@ -0,0 +1,8 @@
+package io.accelerate.solutions.CHK.dictionary;
+
+import java.util.TreeMap;
+
+public class ItemPromoDictionary {
+    public static TreeMap<Character, Integer> itemPriceDictionary = new TreeMap<>();
+
+}
