8d45577a829e6e6f2d714ec1646c509937b67047
sbx_user1051 <sbx_user1051@169.254.66.171>
2025-03-10 12:22:18 +0000

    Mon Mar 10 12:22:18 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,6 +1,8 @@
 package io.accelerate.solutions.CHK;
 
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class CheckoutSolution {
 
@@ -12,14 +14,29 @@ public class CheckoutSolution {
     );
 
     private static final Map<Character, int[]> PROMOTIONS = Map.of(
-            'A', new int[]{3,130},
-            'B', new int[]{2,45}
+            'A', new int[]{3, 130},
+            'B', new int[]{2, 45}
     );
 
+    private static final String VALID_ITEMS_REGEX = "[" +
+            PRICES.keySet().
+                    stream().
+                    map(String::valueOf).
+                    collect(Collectors.joining()) + "]";
+
+    private static final Pattern VALID_ITEMS_PATTERN = Pattern.compile(VALID_ITEMS_REGEX);
+
     public Integer checkout(String skus) {
-        if (skus == null || skus.isBlank()) {
+        if (skus == null || skus.isBlank() || !VALID_ITEMS_PATTERN.matcher(skus.trim()).find()) {
             return -1;
+        }
+
+        for (char c : skus.toCharArray()) {
+            if (PRICES.containsKey(c)) {
+
+            }
         }
     }
 }
+
 
