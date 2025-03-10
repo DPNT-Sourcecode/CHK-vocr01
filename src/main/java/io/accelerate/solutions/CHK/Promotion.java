ae388236d21511d0b92986dc260f2b27f94f9adf
sbx_user1051 <sbx_user1051@169.254.66.171>
2025-03-10 13:20:28 +0000

    Mon Mar 10 13:20:28 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/Promotion.java
+++ src/main/java/io/accelerate/solutions/CHK/Promotion.java
@@ -2,8 +2,25 @@ package io.accelerate.solutions.CHK;
 
 import java.util.Map;
 
+/**
+ * The type Promotion.
+ */
 public abstract class Promotion {
+    /**
+     * Apply the promo to the product based on the check out items and the quantity of the item.
+     *
+     * @param quantity      the quantity of the item to apply the promo
+     * @param checkOutItems the check out items
+     * @return the int [ ] the promos
+     */
     abstract int[] apply(int quantity, Map<Character, Integer> checkOutItems);
+
+    /**
+     * Gets promo.
+     *
+     * @return the promo
+     */
     abstract int getPromo();
 }
+
 
--- src/main/java/io/accelerate/solutions/CHK/QuantityPromotion.java
+++ src/main/java/io/accelerate/solutions/CHK/QuantityPromotion.java
@@ -1,4 +1,48 @@
 package io.accelerate.solutions.CHK;
 
-public class QuantityPromotion {
+import java.util.Map;
+
+/**
+ * The type Quantity promotion.
+ */
+public class QuantityPromotion extends Promotion {
+    private final int quantityNeeded;
+    private final int promoPrice;
+
+    /**
+     * Instantiates a new Quantity promotion.
+     *
+     * @param quantityNeeded the quantity needed
+     * @param promoPrice     the promo price
+     */
+    public QuantityPromotion(int quantityNeeded, int promoPrice) {
+        this.quantityNeeded = quantityNeeded;
+        this.promoPrice = promoPrice;
+    }
+
+
+    /**
+     * Apply the promo to the product based on the check out items and the quantity of the item.
+     *
+     * @param quantity      the quantity of the item to apply the promo
+     * @param checkOutItems the check out items
+     * @return the int [ ] the promos
+     */
+    @Override
+    int[] apply(int quantity, Map<Character, Integer> checkOutItems) {
+        int totalPrice = (quantity / quantityNeeded) * promoPrice;
+
+        return new int[]{totalPrice, quantity % quantityNeeded};
+    }
+
+    /**
+     * Gets promo.
+     *
+     * @return the promo
+     */
+    @Override
+    int getPromo() {
+        return 0;
+    }
 }
+
