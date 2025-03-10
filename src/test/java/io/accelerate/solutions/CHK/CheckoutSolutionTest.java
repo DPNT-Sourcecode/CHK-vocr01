800540bd5781f9a9e52d7d512c3989ab885d45a5
sbx_user1051 <sbx_user1051@169.254.66.171>
2025-03-10 22:47:02 +0000

    Mon Mar 10 22:47:02 UTC 2025

--- src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
@@ -17,14 +17,17 @@ class CheckoutSolutionTest {
     }
 
     @Test
-    void checkoutTest() {
+    void checkoutPhaseOneTest() {
         assertEquals(130, checkoutSolution.checkout("AAA"));
         assertEquals(45, checkoutSolution.checkout("BB"));
 
         assertEquals(95, checkoutSolution.checkout("BDA"));
         assertEquals(245, checkoutSolution.checkout("CCAAABBB"));
         assertEquals(100, checkoutSolution.checkout("AA"));
+    }
 
+    @Test
+    void checkoutPhaseTwoTest() {
         assertEquals(80, checkoutSolution.checkout("EE"));
         assertEquals(80, checkoutSolution.checkout("EEB"));
         assertEquals(110, checkoutSolution.checkout("EEBB"));
@@ -32,7 +35,10 @@ class CheckoutSolutionTest {
         assertEquals(300, checkoutSolution.checkout("AAAAAAA"));
         assertEquals(330, checkoutSolution.checkout("AAAAAAAA"));
         assertEquals(295, checkoutSolution.checkout("CCAAABBBEE"));
+    }
 
+    @Test
+    void checkoutPhaseThreeTest() {
         assertEquals(10, checkoutSolution.checkout("F"));
         assertEquals(20, checkoutSolution.checkout("FF"));
         assertEquals(20, checkoutSolution.checkout("FFF"));
@@ -40,7 +46,26 @@ class CheckoutSolutionTest {
         assertEquals(40, checkoutSolution.checkout("FFFFFF"));
         assertEquals(315, checkoutSolution.checkout("CCAAAFBBBEEF"));
         assertEquals(325, checkoutSolution.checkout("FFCCAAAFBBBEEF"));
+    }
 
+    @Test
+    void checkoutPhaseFourTest() {
+        assertEquals(40, checkoutSolution.checkout("GG"));
+        assertEquals(40, checkoutSolution.checkout("HHHH"));
+        assertEquals(45, checkoutSolution.checkout("HHHHH"));
+        assertEquals(55, checkoutSolution.checkout("HHHHHH"));
+        assertEquals(80, checkoutSolution.checkout("HHHHHHHHHH"));
+
+
+        assertEquals(20, checkoutSolution.checkout("FFF"));
+        assertEquals(30, checkoutSolution.checkout("FFFF"));
+        assertEquals(40, checkoutSolution.checkout("FFFFFF"));
+        assertEquals(315, checkoutSolution.checkout("CCAAAFBBBEEF"));
+        assertEquals(325, checkoutSolution.checkout("FFCCAAAFBBBEEF"));
+    }
+
+    @Test
+    void checkoutInvalidAndZeroTest() {
         assertEquals(-1, checkoutSolution.checkout("&"));
         assertEquals(-1, checkoutSolution.checkout("AB&"));
         assertEquals(-1, checkoutSolution.checkout("&BA"));
@@ -50,3 +75,4 @@ class CheckoutSolutionTest {
         assertEquals(-1, checkoutSolution.checkout(null));
     }
 }
+
