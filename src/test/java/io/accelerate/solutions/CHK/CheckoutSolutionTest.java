82ab13289b5a92c7600e1aa543935818c0e49ebf
sbx_user1051 <sbx_user1051@169.254.66.171>
2025-03-10 12:40:18 +0000

    Mon Mar 10 12:40:18 UTC 2025

--- src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,24 @@
+package io.accelerate.solutions.CHK;
+
+import io.accelerate.solutions.SUM.SumSolution;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CheckoutSolutionTest {
-  
+    private CheckoutSolution checkoutSolution;
+
+    /**
+     * Sets up.
+     */
+    @BeforeEach
+    public void setUp() {
+        checkoutSolution = new CheckoutSolution();
+    }
+
+    @Test
+    void checkoutTest() {
+
+    }
 }
